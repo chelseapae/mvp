44a4f477313389be710995a4e18e4299f638f512
Chelsea Pae <chelseapae@gmail.com>
2022-09-08 14:58:42 -0700

    Complete Search functionality

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,22 +11,52 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    this.getDef();
+  }
+
+  getDef() {
+    $.ajax({
+      type: 'GET',
+      url: '/'
+    })
+    .then(data => {
+      console.log('DATA from client index.jsx', data);
+      this.setState({definition: data})
+    })
+  }
+
+  search (word) {
+    console.log(`${word} was searched`);
+    $.ajax({
+      type: "POST",
+      url: '/',
+      data: {word: word},
+      success: (response)=>{console.log('ajax success', response)},
+      error: (err)=>{console.log('ajax error', err)},
+    })
+      .then(data => {
+        return this.getDef()
+      })
+  }
+
   render () {
     return (
     <div>
-      <h2>Excuse Generator</h2>
-      <p>Need a good excuse? Use our random excuse generator to keep your excuses fresh</p>
+      <h2>Dictionary</h2>
+      <p>Enter in a word and get the definiton</p>
       {/* <Search onSearch={this.search.bind(this)}/> */}
       {/* {exampleData.map((example) => {
         return example.excuse
         })}
       {console.log(exampleData[0].excuse)} */}
+      <Search onSearch={this.search.bind(this)}/>
     </div>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 
 
