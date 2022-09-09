d1b52686a5679f268fe3993df058ca5dd518e89a
Chelsea Pae <chelseapae@gmail.com>
2022-09-09 11:15:02 -0700

    Pivoting projec to not use API

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,59 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import $ from 'jquery';
-import Search from './components/Search.jsx';
-import SearchHistory from './components/SearchHistory.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      words: []
-    }
-  }
-
-  componentDidMount() {
-    this.getDef();
-  }
-
-  getDef() {
-    $.ajax({
-      type: 'GET',
-      url: '/'
-    })
-    .then(data => {
-      // console.log('DATA from client index.jsx', data);
-      this.setState({definition: data})
-    })
-  }
-
-  search (word) {
-    console.log(`${word} was searched`);
-    $.ajax({
-      type: "POST",
-      url: '/',
-      data: {word: word},
-      success: (response)=>{console.log('ajax success', response)},
-      error: (err)=>{console.log('ajax error', err)},
-    })
-      .then(data => {
-        return this.getDef()
-      })
-  }
-
-  render () {
-    return (
-    <div>
-      <h2>Dictionary</h2>
-      <p>Enter in a word and get the definiton</p>
-      <Search onSearch={this.search.bind(this)}/>
-      <SearchHistory words={this.state.words}/>
-    </div>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
